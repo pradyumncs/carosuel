dd0f6d91328063fe4b56253f57d3869729420438
pradyumncs <43007769+pradyumncs@users.noreply.github.com>
2022-06-24 14:40:53 +0530

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Carousel } from 'antd';
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
+const App = () => {
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
+  return (
+    <Carousel afterChange={onChange}>
+      <div>
+        <h3 style={contentStyle}>1</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>2</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>3</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>4</h3>
+      </div>
+    </Carousel>
+  );
+};
+
+export default App;
